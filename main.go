ec71220819ce717be4bf3d4c8fc3886d9c8ba9eb
Vinícius Müller <vinigm.nho@gmail.com>
2023-05-12 20:11:54 -0300

    Run gofmt

--- main.go
+++ main.go
@@ -26,13 +26,13 @@ type Query struct {
 }
 
 func (h Header) Encode() bytes.Buffer {
-  var b bytes.Buffer
-  binary.Write(&b, binary.BigEndian, h.id)
-  binary.Write(&b, binary.BigEndian, h.flags)
-  binary.Write(&b, binary.BigEndian, h.questions)
-  binary.Write(&b, binary.BigEndian, h.answers)
-  binary.Write(&b, binary.BigEndian, h.authorities)
-  binary.Write(&b, binary.BigEndian, h.additionals)
+	var b bytes.Buffer
+	binary.Write(&b, binary.BigEndian, h.id)
+	binary.Write(&b, binary.BigEndian, h.flags)
+	binary.Write(&b, binary.BigEndian, h.questions)
+	binary.Write(&b, binary.BigEndian, h.answers)
+	binary.Write(&b, binary.BigEndian, h.authorities)
+	binary.Write(&b, binary.BigEndian, h.additionals)
 	return b
 }
 
@@ -45,26 +45,25 @@ func (q Query) Encode() bytes.Buffer {
 }
 
 func EncodeDomain(domain string) bytes.Buffer {
-  var b bytes.Buffer
+	var b bytes.Buffer
 
-  for _, part := range strings.Split(domain, ".") {
-    binary.Write(&b, binary.BigEndian, int8(len(part)))
-    b.WriteString(part)
-  }
+	for _, part := range strings.Split(domain, ".") {
+		binary.Write(&b, binary.BigEndian, int8(len(part)))
+		b.WriteString(part)
+	}
 
-  binary.Write(&b, binary.BigEndian, int8(0))
-  return b
+	binary.Write(&b, binary.BigEndian, int8(0))
+	return b
 }
 
-
-// TODO: use "enums" and add other record types, etc 
+// TODO: use "enums" and add other record types, etc
 var TYPE_A int16 = 1
 
 func BuildQuery(domain string, recordType int16) bytes.Buffer {
-  var RECURSION_DESIRED int16 = 1 << 8
-  var CLASS_IN int16 = 1
+	var RECURSION_DESIRED int16 = 1 << 8
+	var CLASS_IN int16 = 1
 
-  flags := RECURSION_DESIRED
+	flags := RECURSION_DESIRED
 
 	header := Header{
 		id:          15,
@@ -75,40 +74,39 @@ func BuildQuery(domain string, recordType int16) bytes.Buffer {
 		additionals: 0,
 	}
 
-  query := Query{
-    name: EncodeDomain(domain),
-    type_: recordType,
-    class: CLASS_IN,
-  }
-  qbuffer := query.Encode()
+	query := Query{
+		name:  EncodeDomain(domain),
+		type_: recordType,
+		class: CLASS_IN,
+	}
+	qbuffer := query.Encode()
 
-  hbuffer := header.Encode()
-  hbuffer.Write(qbuffer.Bytes())
-  return hbuffer
+	hbuffer := header.Encode()
+	hbuffer.Write(qbuffer.Bytes())
+	return hbuffer
 }
 
 func main() {
-  dnsquery := BuildQuery("www.example.com", TYPE_A)
+	dnsquery := BuildQuery("www.example.com", TYPE_A)
 
-  conn, err := net.DialTimeout("udp", "8.8.8.8:53", time.Second * 3)
-  if err != nil {
-      log.Fatalf("Could not open UDP socket: %v", err)
-  }
+	conn, err := net.DialTimeout("udp", "8.8.8.8:53", time.Second*3)
+	if err != nil {
+		log.Fatalf("Could not open UDP socket: %v", err)
+	}
 
-  _, err = conn.Write(dnsquery.Bytes())
-  if err != nil {
-    log.Fatalf("failed to write dns query: %v", err)
-  } 
+	_, err = conn.Write(dnsquery.Bytes())
+	if err != nil {
+		log.Fatalf("failed to write dns query: %v", err)
+	}
 
-  recv := make([]byte, 1024)
+	recv := make([]byte, 1024)
 
-  _, err = conn.Read(recv)
-  if err != nil {
-    log.Fatalf("failed to read response: %v", err)
-  } 
+	_, err = conn.Read(recv)
+	if err != nil {
+		log.Fatalf("failed to read response: %v", err)
+	}
 
+	fmt.Printf("got response: % x\n", recv[:50])
 
-  fmt.Printf("got response: % x\n", recv[:50])
-
-  conn.Close()
+	conn.Close()
 }
